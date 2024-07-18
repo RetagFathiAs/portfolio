cbc290493901d6c70fe60afdff7fc789bbd8f2ec
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-07-19 01:00:31 +0200

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import csharp from "./tech/csharp.png";
+import phot from "./tech/phot.png";
+import sqlserver from "./tech/sqlserver.png";
+import python from "./tech/python.png";
+import inkscape from "./tech/inkscape.png";
+
+
+
+import coding from "./company/coding.png";
+import cicon from "./company/cicon.png";
+import java from "./company/java.png";
+import icdl from "./company/icdl.png";
+import alx from "./company/alx.png";
+import code from "./company/code.png";
+import server from "./company/server.png";
+import photoshop from "./company/photoshop.png";
+
+import carrent from "./website.png";
+import jobit from "./dragon.png";
+import tripguide from "./pharmacy.png";
+
+export {
+  inkscape,
+  python,
+  sqlserver,
+  phot,
+  csharp,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  icdl,
+ java,
+  alx,
+  coding,
+  cicon,
+  code,
+  server,
+  photoshop,
+  carrent,
+  jobit,
+  tripguide,
+};
